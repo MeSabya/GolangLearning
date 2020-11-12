e5b8744df432444f04f461927fa8a49793651e09
sabyasachi nayak <nayak.saabyasachi@gmail.com>
2020-11-12 23:22:43 +0530

    WAitgroup understanding

--- Concurrency/waitGroup.go
+++ Concurrency/waitGroup.go
@@ -6,6 +6,13 @@ import (
 	"time"
 )
 
+/*
+What is a WaitGroup? #
+A WaitGroup blocks a program and waits for a set of goroutines to finish before
+moving to the next steps of execution.
+
+*/
+
 func worker(id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Printf("Worker %d starting\n", id)
